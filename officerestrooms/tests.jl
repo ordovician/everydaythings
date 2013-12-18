16e02f7ec5e92cbfcafa1d9c91f88e83e60fa670
Erik Engheim <erik.engheim@mac.com>
2013-12-18 22:29:47 +0100

    failed attempt at figuring out what is going wrong
    Tried an alternative implementation of the algorithm, but that did not seem to help.

--- officerestrooms/tests.jl
+++ officerestrooms/tests.jl
@@ -2,13 +2,6 @@ using Office
 
 const duration = 10
 
-restroom = Restroom(15)
-
-println("population size: $(length(restroom.population))")
-println("facilities sizee: $(length(restroom.facilities))")
-println("-------------------------------------------------")
-
-pop = restroom.population
 # queue = restroom.queue
 
 
@@ -23,26 +16,125 @@ pop = restroom.population
 # indicies = find(facility -> Office.occupied(facility), restroom.facilities)
 # println("p: $(length(pop)) q: $(length(queue)) o: $(length(indicies))")
 
-function no_occupied(room::Restroom)
-    indicies = find(facility -> Office.occupied(facility), restroom.facilities)
-    length(indicies)
- end
 
-for t in 1:duration
-    no_occupied_restrooms = no_occupied(restroom)
-    println("$t p: $(length(pop)) q: $(length(restroom.queue)) o: $no_occupied_restrooms")
+
+
+function test_alt()
+    const freq = 3
+    const no_facilities = 3
+    const use_duration = 2
     
-    queue = copy(restroom.queue)
-    empty!(restroom.queue)
+    queue_length = 0
+    population  = 80
+    facilities = Array(Int64, 0)
     
-    if !isempty(queue)
-        enter!(restroom, shift!(queue))
-    end
-    for person in restroom.population
-        if need_to_go(person, duration)
-            enter!(restroom, person)
+    const population_range = 10:10:600
+    
+    # println("population size: $population")
+    # println("facilities sizee: $no_facilities")
+    # println("-------------------------------------------------")
+    
+    data = Array(Int32, (duration, length(population_range)))
+    for population_size in population_range
+        for t in 1:duration
+            popcount = population
+            for i in 1:popcount
+                if rand(1:duration) <= freq
+                    queue_length += 1
+                    population -= 1
+                end           
+            end 
+        
+            for i in 1:length(facilities)
+               facilities[i] -= 1
+               @assert facilities[i] >= 0
+            end
+        
+            while !isempty(facilities) && first(facilities) == 0
+                shift!(facilities)
+                population += 1
+            end
+        
+            while queue_length > 0 && length(facilities) < no_facilities
+                push!(facilities, use_duration)
+                queue_length -= 1
+            end
+            data[t,population_size/step(population_range)] = queue_length
+            # println("p: $population q: $queue_length o: $(length(facilities))")
         end
     end
-    tick!(restroom)
     
+    csv = Array(Int32, (duration + 1, length(population_range)))
+    csv[1,:] = population_range
+    csv[2:end,:] = data
+
+    writecsv("simulation1.csv", csv)
 end
+
+
+function test_occupation()
+    restroom = Restroom(20)
+    function no_occupied(room::Restroom)
+        indicies = find(facility -> Office.occupied(facility), restroom.facilities)
+        length(indicies)
+    end
+    println("population size: $(length(restroom.population))")
+    println("facilities sizee: $(length(restroom.facilities))")
+    println("-------------------------------------------------")
+    
+    for t in 1:duration
+        
+    
+        queue = copy(restroom.queue)
+        empty!(restroom.queue)
+    
+        if !isempty(queue)
+            enter!(restroom, shift!(queue))
+        end
+        for person in restroom.population
+            if need_to_go(person, duration)
+                enter!(restroom, person)
+            end
+        end
+        
+        no_occupied_restrooms = no_occupied(restroom)
+        println("$t p: $(length(restroom.population)) q: $(length(restroom.queue)) o: $no_occupied_restrooms")        
+        tick!(restroom)
+    
+    end
+end
+
+
+function test_need_to_go()
+    const freq = 4
+    const use_duration = 1   
+   
+    p = Person(freq, use_duration)
+
+    visits = [need_to_go(p, 10) ? 1 : 0 for i in 1:1000]
+    print("visits after 100 ticks:")
+    println(sum(visits))    
+end
+
+
+function test_tick()
+    const freq = 4
+    const use_duration = 1
+    const population_size = 2
+    
+    population = Set{Person}() 
+    union!(population, [Person(freq, use_duration) for i in 1:population_size])
+    
+    f = Facility()
+    occupy!(f, first(population), population)
+    for i in 1:10
+        println("$(Office.occupied(f))")
+        tick!(f, population)
+    end
+
+end
+
+test_alt()
+# test_occupation()
+# test_tick()
+
