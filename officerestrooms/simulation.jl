99a32e652d0e7b3961d0a4a442ea7b8dacb5bc0d
Erik Engheim <erik.engheim@mac.com>
2013-12-18 23:59:13 +0100

    Don't try to make julia act like ruby with arrays
    Once again I see that I get shorter code by doing things in a more julia typical fashion rather than making array within arrays or arrays within dictionaries.
    
    This is also probably easier to read. So avoid dynamically creating arrays to the same extend as in ruby and instead preallocate arrays of the correct size and dimension.

--- officerestrooms/simulation.jl
+++ officerestrooms/simulation.jl
@@ -4,23 +4,15 @@ const population_range = 10:10:600
 const DURATION = 540
 
 # maps population size to a restroom queue sizes
-data = Dict{Int64, Vector{Int64}}()
+data = Array(Int64, (DURATION, length(population_range)))
 
 for population_size in population_range
     restroom = Restroom(population_size)
-    data[population_size] = queue_sizes_after(restroom, DURATION)    
+    data[:,population_size/step(population_range)] = queue_sizes_after(restroom, DURATION)    
 end
-
 
 csv = Array(Int32, (DURATION + 1, length(population_range)))
 csv[1,:] = population_range
-
-for t in 1:DURATION
-    column = 1
-    for population_size in population_range
-      csv[t+1, column] = data[population_size][t]
-      column += 1
-    end 
-end
+csv[2:end,:] = data
 
 writecsv("simulation1.csv", csv)
